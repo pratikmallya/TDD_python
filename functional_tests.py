50a2a41a9430f2903d9f5d51c4450c5c855ca1ff
pratikmallya <pratik.mallya@gmail.com>
2014-08-11 12:51:52 -0500

    ADD: Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -20,7 +20,26 @@ class NewVisitorTest(unittest.TestCase):
 		# she notices the page title and header mention to-do
 		# lists
 		self.assertIn('To-Do', self.browser.title)
-		self.fail('Finish the test!')
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)	
 
+		# she is invited to enter a to-do list right away
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+		)
+
+		inputbox.send_keys('Buy peacock feathers')
+
+		inputbox.send_keys(Keys.ENTER)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_element_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Buy peacock feathers' for row in rows)
+		)
+		self.fail('Finish the test!')
+		
 if __name__ == '__main__':
 	unittest.main(warnings='ignore')
