aceb6ce67e3e3f141ebc7b58bd1c55fc4ed72d5d
pratikmallya <pratik.mallya@gmail.com>
2014-08-11 13:29:12 -0500

    ADD: front page HTML now generated from template

--- functional_tests.py
+++ functional_tests.py
@@ -37,9 +37,10 @@ class NewVisitorTest(unittest.TestCase):
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_element_by_tag_name('tr')
 		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows)
+			any(row.text == '1: Buy peacock feathers' for row in rows),
+			"New to-do item did not appear in the table"
 		)
 		self.fail('Finish the test!')
-		
+
 if __name__ == '__main__':
 	unittest.main(warnings='ignore')
