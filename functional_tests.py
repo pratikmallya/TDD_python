ede4901dcf7d28028a53a5c8e5d3163f6aa214bb
pratikmallya <pratik.mallya@gmail.com>
2014-08-13 00:39:36 -0500

    FIX: refactor

--- functional_tests.py
+++ functional_tests.py
@@ -4,53 +4,54 @@ import unittest
 
 class NewVisitorTest(unittest.TestCase):
 
-	def setUp(self):
-		self.browser = webdriver.Firefox()
-		self.browser.implicitly_wait(3)
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
 
-	def tearDown(self):
-		self.browser.quit()
+    def tearDown(self):
+        self.browser.quit()
 
-	def test_can_start_a_list_and_retrieve_it_later(self):
-		# Edith has heard about a cool new online app. She
-		# goes to check out its homepage
-		self.browser.get('http://localhost:8000')
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
-		# she notices the page title and header mention to-do
-		# lists
-		self.assertIn('To-Do', self.browser.title)
-		header_text = self.browser.find_element_by_tag_name('h1').text
-		self.assertIn('To-Do', header_text)
 
-		# she is invited to enter a to-do list right away
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		self.assertEqual(
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online app. She
+        # goes to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # she notices the page title and header mention to-do
+        # lists
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # she is invited to enter a to-do list right away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
-		)
+        )
 
-		inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys('Buy peacock feathers')
 
-		inputbox.send_keys(Keys.ENTER)
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		inputbox.send_keys('Use peacock feathers to make a fly')
-		inputbox.send_keys(Keys.ENTER)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-		self.assertIn(
-			'2: Use peacock feathers to make a fly',
-			[row.text for row in rows]
-		)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table(
+            '2: Use peacock feathers to make a fly')
 
-		self.fail('Finish the test!')
+        self.fail('Finish the test!')
+
 
 
 if __name__ == '__main__':
-	unittest.main(warnings='ignore')
+    unittest.main(warnings='ignore')
